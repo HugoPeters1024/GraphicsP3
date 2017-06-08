44cc43b86eb23744a55f12c3726ee033e99fd910
Maxymulator <maxvangogh@home.nl>
2017-06-08 16:15:42 +0200

    Changes from the slides
    Added lightsources and simple shading.
    All based on code provided in the lectures.

---
+++ light.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+
+namespace template_P3
+{
+    class Light
+    {
+        Vector3 position;
+        Vector3 intensity;
+
+        public Light(Vector3 position)
+        {
+            this.position = position;
+            intensity = new Vector3(1f);
+        }
+
+        public Light(Vector3 position, Vector3 intensity)
+        {
+            this.position = position;
+            this.intensity = intensity;
+        }
+
+        public Light(Vector3 position, float intensity)
+        {
+            this.position = position;
+            this.intensity = new Vector3(intensity);
+        }
+
+        #region Properties
+        public virtual Vector3 Intensity
+        {
+            get { return intensity; }
+            set { intensity = value; }
+        }
+
+        public Vector3 Origin
+        {
+            get { return position; }
+            set { position = value; }
+        }
+        #endregion
+    }
+}
